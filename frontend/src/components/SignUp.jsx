59a3555813702ebe4b6fbf5f5b523a9c4ab8cf45
ChathurK <kumaras-im21001@stu.kln.ac.lk>
2025-04-02 17:14:03 +0530

    update SignUp component styling, update projectStructure

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -24,7 +24,7 @@ const SignUp = () => {
       className="flex justify-center items-center min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${assets.signUpBg})` }}
     >
-      <div className="bg-white bg-opacity-50 backdrop-blur-sm p-8 rounded-lg shadow-lg w-96">
+      <div className="bg-white bg-opacity-50 backdrop-blur-sm p-8 rounded-lg shadow-lg w-auto">
         <h2 className="text-2xl font-bold text-center text-gray-700 mb-4">Sign Up</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           {/* First Name and Last Name in one row */}
@@ -82,15 +82,22 @@ const SignUp = () => {
           </div>
           <div>
             <label className="block text-gray-600">Role</label>
-            <select
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              required
-            >
-              <option value="Admin">Admin</option>
-              <option value="Employee">Employee</option>
-            </select>
+            <div className="relative z-20">
+              <select
+                className="w-full px-3 py-2  appearance-none border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                required
+              >
+                <option value="Admin">Admin</option>
+                <option value="Employee">Employee</option>
+              </select>
+              <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+              </span>
+            </div>
           </div>
           <button
             type="submit"
